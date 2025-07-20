2259203ad620e8274864424de0c5ac4e3c2ce4ee
ItishaJain123 <jainitisha192@gmail.com>
2025-07-20 15:45:41 +0530

    Update: Input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,18 @@
 const Input = ({ label, placeholder, state, setState, type }) => {
   return (
     <div className="flex flex-col gap-1 mb-4">
-      <label className="text-sm font-medium text-white">{label}</label>
+      <label className="text-sm font-medium text-gray-800 dark:text-white">
+        {label}
+      </label>
       <input
         value={state}
         placeholder={placeholder}
         type={type}
         onChange={(e) => setState(e.target.value)}
-        className="px-4 py-2 border border-white/30 bg-transparent text-white placeholder-white/70 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent text-sm"
+        className="px-4 py-2 border border-gray-300 dark:border-white/30
+                   bg-white text-gray-800 placeholder-gray-500
+                   dark:bg-transparent dark:text-white dark:placeholder-white/70
+                   rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent text-sm"
       />
     </div>
   );
